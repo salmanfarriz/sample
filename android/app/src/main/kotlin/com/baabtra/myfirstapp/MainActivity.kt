08c95a26cc93a8d65fb3052956f76082457764d6
salmanfarriz <salmanfariz.pp@gmail.com>
2022-04-28 12:27:06 +0530

    commit 3

---
+++ android/app/src/main/kotlin/com/baabtra/myfirstapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.baabtra.myfirstapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'First App',
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text('SALMAN'),
+          ),
+        ),
+      ),
+    ),
+  ); //for Run the App
+}
+
